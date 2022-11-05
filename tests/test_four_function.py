3ec2ae75e85931cd3df8650a56370d0a85d37334
huyta55 <huyta55@gmail.com>
2022-11-04 21:29:03 -0400

    Fixed some flake8 issues

--- tests/test_four_function.py
+++ tests/test_four_function.py
@@ -1,6 +1,6 @@
 from four_func_calculator.four_function import four_function
 
 def test_four_function():
-    assert four_function(2, 3, 1) is 5
-    assert four_function(2, 3, 2) is not 5
+    assert four_function(2, 3, 1) == 5
+    assert four_function(2, 3, 2) != 5
     
