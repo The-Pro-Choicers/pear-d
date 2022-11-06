ea87ef8a3a7d91ae8ff7738cfc245764afb74c4b
huyta55 <huyta55@gmail.com>
2022-11-05 20:17:27 -0400

    Changed logic order of database creation

--- src/peard/connect_db.py
+++ src/peard/connect_db.py
@@ -5,7 +5,7 @@ import uuid
 logger = logging.getLogger(__name__)
 
 
-def doUserLogin(cursor: pymysql.cursors.DictCursor, username: str, password: str) -> None:
+def doUserLogin(cursor: pymysql.cursors.DictCursor, username: str, password: str):
     # Gets the table
     current_id = None
     while True:
@@ -13,11 +13,7 @@ def doUserLogin(cursor: pymysql.cursors.DictCursor, username: str, password: str
             query = f"select uid from peard.usercredentials where username=\"{username}\" and password=\"{password}\""
             cursor.execute(query)
             current_id = cursor.fetchone()
-            if current_id is None:
-                logger.error("The username or password provided does not match our records.")
-            else:
-                print(current_id['uid'])
-            break
+            return current_id
         except pymysql.ProgrammingError as e:
             logger.error("The necessary login data tables does not exists")
             raise e
@@ -44,14 +40,13 @@ def doUserRegistration(cursor: pymysql.cursors.DictCursor, email: str, username:
 def createDatabase(cursor: pymysql.cursors.DictCursor) -> None:
     while True:
         try:
-            cursor.execute("create database peard")
-            cursor.connection.commit()
+            cursor.execute("use peard")
             break
-        except pymysql.Error as e:
-            if e.args[0] == 1007:
-                cursor.execute("use peard")
-                logging.info("Database peard already found, using existing one.")
-                break
+        except pymysql.OperationalError as e:
+            if e.args[0] == 1049:
+                cursor.execute("create peard")
+                cursor.connection.commit()
+                logging.info(f"No database found, creating one.")
             else:
                 raise e
     # Loading user credentials table
@@ -88,21 +83,8 @@ def connectDB(hostname: str, usr: str, passwd: str):
         db = None
     return db
 
-def showTable(cursor: pymysql.cursors.DictCursor, table_name: str) -> None:
-    # Used for development purposes only
-    try:
-        query = f"select * from {table_name}"
-        cursor.execute(query)
-        print("-----------------------------------------")
-        print(cursor.fetchall())
-    except pymysql.ProgrammingError as e:
-        print(f"couldn't find table with name: {table_name}")
-        raise e
-
 
 def main() -> None:
-    print("")
-    print("-----------------------------------------")
     db = connectDB(
         hostname="peard-database.cbiya7huefjt.us-east-1.rds.amazonaws.com",
         usr="pearddev",
@@ -110,8 +92,6 @@ def main() -> None:
         )
     cursor = db.cursor(pymysql.cursors.DictCursor)
     createDatabase(cursor=cursor)
-    doUserLogin(cursor=cursor, username="huyta55", password="huytapassword55")
-    # showTable(cursor=cursor, table_name="peard.usercredentials")
 
 
 if __name__ == "__main__":
