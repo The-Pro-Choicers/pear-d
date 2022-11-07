8bde78bf1117815ac7674fadf5048f0a1d7a85b2
huyta55 <huyta55@gmail.com>
2022-11-07 16:25:25 -0500

    Set up authentication API endpoints

---
+++ pear_d_backend/api/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+import uuid
+
+def generate_user_id():
+    return uuid.uuid4().hex
+
+# Create your models here.
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("Email address cannot be empty")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_dev = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
+
