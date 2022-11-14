961e20c7926e95944407a89c2592bdb9ceb4c236
huyta55 <huyta55@gmail.com>
2022-11-14 15:56:48 -0500

    Added some tests

--- pear_d_backend/tests/test_authmodels.py
+++ pear_d_backend/tests/test_authmodels.py
@@ -1,7 +1,43 @@
 import pytest
 from api.models import UserAccount
+from rest_framework.test import APIClient
+from faker import Faker
+
+client = APIClient()
+faker = Faker()
 
 # Test creation and database insertion of a single user
 def test_new_user(new_user):
     assert new_user.email
     assert UserAccount.objects.all().count() == 1
+
+
+@pytest.mark.django_db
+def test_register_user_request():
+    passwd = faker.password()
+    payload = {
+        "email": faker.email(),
+        "name": faker.name(),
+        "password": passwd,
+        "re_password": passwd,
+    }
+
+    response = client.post("/auth/users/", payload)
+    data = response.data
+
+    assert data["email"] == payload["email"]
+    assert data["name"] == payload["name"]
+    assert payload["password"] not in data
+    assert type(data["id"]) is int
+
+
+# @pytest.mark.django_db
+# def test_user_login():
+#     # Testing successful user login
+#     passwd = faker.password()
+#     payload = {
+#         "email": faker.email(),
+#         "name": faker.name(),
+#         "password": passwd,
+#         "re_password": passwd,
+#     }
