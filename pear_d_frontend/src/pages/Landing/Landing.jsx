34153a8190ace72c09e119e61453b5e1888b226f
Brianna Rodriguez <brianna.rodriguez2240@gmail.com>
2022-11-16 16:31:53 -0500

    adding to logans aesthetics and fixing media queries

--- pear_d_frontend/src/pages/Landing/Landing.jsx
+++ pear_d_frontend/src/pages/Landing/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import pear from '../../assets/images/landing.png';
+import Spline from '@splinetool/react-spline';
 import { useNavigate } from 'react-router-dom';
 import './Landing.css'
 
@@ -7,16 +8,16 @@ const Landing = () => {
   const navigate = useNavigate();
   return (
     <div className="landing">
-        <div className="intro">
+      <div className="intro">
                 <h1>Pear'd</h1>
+                <div className="image">
+                  <Spline scene="https://prod.spline.design/w8cAx66muvmkx9GV/scene.splinecode" />
+                </div>
                 <h2>
                     Connecting you with restaurants that care.
                 </h2>
-                <button onClick={() => navigate('/login')}>Login</button>           
-        </div>
-        <div className="image">
-            <img src={pear}/>
-        </div>
+                <button onClick={() => navigate('/login')}>Login</button>                     
+      </div>
     </div>
   )
 }
