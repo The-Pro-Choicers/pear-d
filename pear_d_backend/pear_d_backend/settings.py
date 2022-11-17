66513943d9a202e6738a9af5f3758cb05453c48f
huyta55 <huyta55@gmail.com>
2022-11-17 00:10:16 -0500

    Fixed os path joining for the static files

--- pear_d_backend/pear_d_backend/settings.py
+++ pear_d_backend/pear_d_backend/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR.parent,"pear_d_frontend", "build\static")
+    os.path.join(BASE_DIR.parent,"pear_d_frontend", "build", "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
