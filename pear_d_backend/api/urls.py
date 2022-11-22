febfbe05cab0bc5965394bce2f9c5b2fcc7a3af5
huyta55 <huyta55@gmail.com>
2022-11-22 02:40:31 -0500

    Adding changes the the views and urls

--- pear_d_backend/api/urls.py
+++ pear_d_backend/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import RestaurantView, UserAccountAllView, UserAccountUpdateView, UserAccountRetrieveView, FavoritesUpdateView, UserFavoritesDeleteView
+from .views import RestaurantListAllView, RestaurantDetailedView, FindRestaurantView, UserAccountAllView, UserAccountUpdateView, UserAccountRetrieveView, FavoritesUpdateView, UserFavoritesDeleteView
 
 urlpatterns = [
-    path('restaurants', RestaurantView.as_view()),
+    path('restaurants/all', RestaurantListAllView.as_view()),
+    path("restaurants/<int:id>/", RestaurantDetailedView.as_view()),
+    path("restaurants/pl=<int:price>env=<int:env_conscious>ph=<int:philanthropic>m=<int:minority>/", FindRestaurantView.as_view()),
+    path("restaurants/pl=<int:price>ph=<int:philanthropic>m=<int:minority>/", FindRestaurantView.as_view()),
     path("profile/admin/all/", UserAccountAllView.as_view()),
     path("profile/myprofile/update/", UserAccountUpdateView.as_view()),
     path("profile/myprofile/view/", UserAccountRetrieveView.as_view()),
--- pear_d_backend/api/views.py
+++ pear_d_backend/api/views.py
@@ -8,11 +8,85 @@ from django.db.models.expressions import Func
 
 
 # Restaurant View
-class RestaurantView(generics.ListAPIView):
-    permission_classes = [permissions.IsAdminUser,]
+class RestaurantListAllView(generics.ListAPIView):
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAdminUser, permissions.IsAuthenticated]
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
 
+
+class RestaurantDetailedView(
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+    ):
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAdminUser, permissions.IsAuthenticated]
+    queryset = Restaurant.objects.all()
+    serializer_class=RestaurantSerializer
+    lookup_field = "id"
+
+    def get(self, request, *args, **kwargs):
+        id = kwargs.get("id")
+        if id is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+
+class FindRestaurantView(
+    mixins.ListModelMixin,
+    generics.GenericAPIView
+):
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAdminUser, permissions.IsAuthenticated]
+    
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+        
+
+    def get_queryset(self):
+        kwargs = self.kwargs
+        print(kwargs)
+        env_conscious_int = kwargs.get("env_conscious")
+        philanthropic_int = kwargs.get("philanthropic")
+        minority_int = kwargs.get("minority")
+        price_int = kwargs.get("price")
+
+        if env_conscious_int is not None:
+            if env_conscious_int > 1 or env_conscious_int < 0:
+                env_conscious_int = 0
+        
+        if philanthropic_int is not None:
+            if philanthropic_int > 1 or philanthropic_int < 0:
+                philanthropic_int = 0
+        
+        if minority_int is not None:
+            if minority_int > 1 or minority_int < 0:
+                minority_int = 0
+
+        qs = None
+        # Conditionals for whether we are filtering each of the social filters or not
+        if env_conscious_int is not None and philanthropic_int is not None and minority_int is not None and price_int is not None:
+            qs = super().get_queryset().filter(
+                price_level__exact=price_int,
+                env_conscious__exact=bool(env_conscious_int),
+                minority__exact=bool(minority_int),
+                philanthropic__exact=bool(philanthropic_int),
+                )
+        # No env conscious
+        elif philanthropic_int is not None and minority_int is not None and price_int is not None:
+            qs = super().get_queryset().filter(
+                price_level__exact=price_int,
+                minority__exact=bool(minority_int),
+                philanthropic__exact=bool(philanthropic_int),
+                )
+        else:
+            qs = Restaurant.objects.none()
+        return qs
+
+        
 class UserAccountAllView(generics.ListAPIView):
     permission_classes = [permissions.IsAdminUser,]
     queryset = UserAccount.objects.all()
