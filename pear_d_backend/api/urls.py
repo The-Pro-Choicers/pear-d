f43cc5d2849875c132617f80e85d20eafe6097ef
huyta55 <huyta55@gmail.com>
2022-11-22 02:40:31 -0500

    Adding changes the the views and urls

--- pear_d_backend/api/urls.py
+++ pear_d_backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RestaurantView
+from .views import RestaurantListAllView, RestaurantDetailedView, FindRestaurantView
 
 urlpatterns = [
-    path('restaurants', RestaurantView.as_view())
+    path('restaurants/all', RestaurantListAllView.as_view()),
+    path("restaurants/<int:id>/", RestaurantDetailedView.as_view()),
+    path("restaurants/pl=<int:price>env=<int:env_conscious>ph=<int:philanthropic>m=<int:minority>/", FindRestaurantView.as_view()),
+    path("restaurants/pl=<int:price>ph=<int:philanthropic>m=<int:minority>/", FindRestaurantView.as_view())
 ]
--- pear_d_backend/pear_d_backend/urls.py
+++ pear_d_backend/pear_d_backend/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("auth/", include('djoser.urls')),
     path("auth/", include("djoser.urls.jwt")),
     path("api/", include('api.urls'))
