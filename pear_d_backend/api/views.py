28ed992501e83d12ae44cced67c9c1052d026023
huyta55 <huyta55@gmail.com>
2022-11-22 00:17:59 -0500

    Added initial stuff

--- pear_d_backend/api/views.py
+++ pear_d_backend/api/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import response
+from rest_framework.decorators import api_view
+from .serializers import UserProfileSerializer
+from .models import UserProfile
 # importing the restaurant serializer and its model
 from .serializers import RestaurantSerializer
 from .models import Restaurant
@@ -8,3 +11,8 @@ from .models import Restaurant
 class RestaurantView(generics.CreateAPIView):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+
+class UserProfileView(generics.CreateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+
