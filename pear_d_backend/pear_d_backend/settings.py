c3457484979a90bfdb1b19808ce613267422e2da
Huy Ta <huyta55@gmail.com>
2022-11-24 14:13:08 -0500

    PC-135 Fixed Bug w/ Authentication (#21)

--- pear_d_backend/pear_d_backend/settings.py
+++ pear_d_backend/pear_d_backend/settings.py
@@ -153,6 +153,7 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("JWT",),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    "USER_ID_FIELD": "email"
 
 }
 
