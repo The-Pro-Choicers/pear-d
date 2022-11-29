4af78495d5ffaf9e686a4c1d5c7d3b4908b4629a
Huy Ta <huyta55@gmail.com>
2022-11-28 23:59:28 -0500

    PC-132 Add Restaurant endpoints (#24)

--- pear_d_backend/api/urls.py
+++ pear_d_backend/api/urls.py
@@ -1,8 +1,24 @@
 from django.urls import path
-from .views import RestaurantView, UserAccountAllView, UserAccountUpdateView, UserAccountRetrieveView, FavoritesUpdateView, UserFavoritesDeleteView
+from .views import RestaurantListAllView, RestaurantDetailedView, FindRestaurantView, UserAccountAllView, UserAccountUpdateView, UserAccountRetrieveView, FavoritesUpdateView, UserFavoritesDeleteView
 
 urlpatterns = [
-    path('restaurants', RestaurantView.as_view()),
+    path('restaurants/all', RestaurantListAllView.as_view()),
+    path("restaurants/detailed/<int:id>/", RestaurantDetailedView.as_view()),
+    path("restaurants/socialfilter/e=<int:env_conscious>ph=<int:philanthropic>m=<int:minority>p=<int:price>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/ph=<int:philanthropic>m=<int:minority>p=<int:price>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/e=<int:env_conscious>m=<int:minority>p=<int:price>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/e=<int:env_conscious>ph=<int:philanthropic>p=<int:price>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/e=<int:env_conscious>ph=<int:philanthropic>m=<int:minority>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/e=<int:env_conscious>ph=<int:philanthropic>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/e=<int:env_conscious>m=<int:minority>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/e=<int:env_conscious>p=<int:price>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/ph=<int:philanthropic>m=<int:minority>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/ph=<int:philanthropic>p=<int:price>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/m=<int:minority>p=<int:price>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/e=<int:env_conscious>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/ph=<int:philanthropic>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/m=<int:minority>/", FindRestaurantView.as_view()),
+    path("restaurants/socialfilter/p=<int:price>/", FindRestaurantView.as_view()),
     path("profile/admin/all/", UserAccountAllView.as_view()),
     path("profile/myprofile/update/", UserAccountUpdateView.as_view()),
     path("profile/myprofile/view/", UserAccountRetrieveView.as_view()),
