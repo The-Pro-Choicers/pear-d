a738c8e0f31536c82453aa0d47edcbf770da99be
Brianna Rodriguez <brianna.rodriguez2240@gmail.com>
2022-11-30 20:11:55 -0500

    displayed all of the restaurant data on the screen besides the picture because the json returns a string not an actual url

--- pear_d_frontend/src/pages/Restaurants.js
+++ pear_d_frontend/src/pages/Restaurants.js
@@ -4,14 +4,17 @@ import { connect } from 'react-redux';
 import styled from "styled-components";
 
 const Restaurants = ({ getAll }) => {
-    const [allRestaurants, setAllRestaurants] = useState(true);
+    const [restaurants, setRestaurants] = useState([]);
 
     const onSubmit = e => {
        console.log("button clicked");
-       setAllRestaurants(false);
         e.preventDefault();
-        getAll();
-
+        getAll()
+        .then(function(result) {
+            console.log(result);
+            setRestaurants(result);
+        });
+        
     }
 
   return (
@@ -39,21 +42,41 @@ const Restaurants = ({ getAll }) => {
                 <option value={1}>$</option>
                 <option value={2}>$$</option>
                 <option value={3}>$$$</option>
+                <option value={4}>$$$$</option>
             </select>
         </Dropdown>
         <Dropdown>
             <select className="drop" name="social" id="social-good">
                 <option value="default" disabled selected>social good</option>
                 <option value={1}>eco-concious</option>
-                <option value={2}>minority owned</option>
-                <option value={3}>philanthropist</option>
+                <option value={1}>minority owned</option>
+                <option value={1}>philanthropist</option>
             </select>
         </Dropdown>
         </Wrapper>
         <button type="submit">apply</button>
       </form>
-        
+      { restaurants.map(restaurant => (
+        <RestaurantBubble>
+            <div className="item" key={restaurant.id}>
+            <div className="rest-img app__flex">
+              <img src={restaurant.photo_ref} alt={restaurant.name} />
+            </div>
+            <div className="rest-content app__flex">
+              <h4>{restaurant.name}</h4>
+              <p className="p-text app__flex" style={{ marginTop: 10 }}>Address:   {restaurant.address}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>Price Level:   {restaurant.price_level}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>Rating:   {restaurant.rating}</p>              
+              <a href={restaurant.url} className="p-text" style={{ marginTop: 10 }}>Let's Go!</a>
+              <div className="rest-tag app__flex">
+                <p className="p-text" style={ {color: "black"}}>{restaurant.food_category}</p>
+              </div>
+            </div>
+          </div>
+        </RestaurantBubble>
+      ))}
     </>
+    
   )
 }
 
@@ -79,6 +102,102 @@ const Dropdown = styled.div`
        margin: 30px;
     }
     
+
+`;
+
+const RestaurantBubble = styled.div`
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+    .p-text {
+        font-size: 0.8rem;
+        text-align: left;
+        color: var(--gray-color);
+        line-height: 1.5;
+      
+        @media screen and (min-width: 2000px) {
+          font-size: 1.75rem;
+        }
+    }
+
+    .app__flex {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .item {
+        width: 270px;
+        flex-direction: column;
+
+        margin: 2rem;
+        padding: 1rem;
+        border-radius: 0.5rem;
+        background-color: #96aa56;
+        color: #fff;
+
+        cursor: pointer;
+        transition: all 0.3s ease;
+
+        &:hover {
+        box-shadow: 0 0 25px rgba(0, 0, 0, 0.2);
+        }
+
+        @media screen and (min-width: 2000px) {
+        width: 470px;
+        padding: 1.25rem;
+        border-radius: 0.75rem;
+        }
+
+        @media screen and (max-width: 300px) {
+        width: 100%;
+        margin: 1rem;
+        }
+    }
+
+    .rest-img {
+        width: 100%;
+        height: 230px;
+      
+        position: relative;
+      
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 0.5rem;
+          object-fit: cover;
+        }
+      
+        @media screen and (min-width: 2000px) {
+          height: 350px;
+        }  
+    }
+ 
+    .rest-content {
+        padding: 0.5rem;
+        width: 100%;
+        position: relative;
+        flex-direction: column;
+      
+        h4 {
+          margin-top: 1rem;
+          line-height: 1.5;
+      
+          @media screen and (min-width: 2000px) {
+            margin-top: 3rem;
+          }       
+    }
+
+    .rest-tag {
+        position: absolute;
+        padding: 0.5rem 1rem;
+        border-radius: 10px;
+        background-color: #fff;
+        top: -35px;           
+    }
 
 `;
 
